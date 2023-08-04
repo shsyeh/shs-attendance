cd8f16ef66af10852f600c4635d3530e576c9b40
WY <byoink@gmail.com>
2023-08-04 08:53:25 -0700

    rename app folders

--- shs/shs/settings.py
+++ shs/shs/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'shs.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'shs.wsgi.application'
 
 
 # Database
